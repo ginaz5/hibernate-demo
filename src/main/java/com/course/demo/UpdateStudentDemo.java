2d7e71c8c80ca5e6fa140f25380fe9b3aee83533
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-10-13 08:38:58 +0800

    Update data

---
+++ src/main/java/com/course/demo/UpdateStudentDemo.java
@@ -0,0 +1,53 @@
+package com.course.demo;
+
+import com.course.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class UpdateStudentDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                                                    .addAnnotatedClass(Student.class)
+                                                    .buildSessionFactory();
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try
+        {
+            int studentId = 1;
+
+            // get a new session and transaction and retrieve student: primary key
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            System.out.println("Getting Student with id: " + studentId);
+            Student myStudent = session.get(Student.class, studentId);
+
+            System.out.println("===> Updating student ....");
+            myStudent.setFirstname("Scooby");
+            session.getTransaction().commit();
+
+
+            // new transaction
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            // update email for all students
+            System.out.println("===? update email for all students");
+            session.createQuery("update Student set email='foo@gmail.com'").executeUpdate();
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+        }
+        finally {
+            factory.close();
+        }
+
+    }
+
+}
