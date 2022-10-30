c5791729462a77150dbd72655faa569520f5cf14
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-10-30 14:13:42 +0800

    eager loading demo

---
+++ src/main/java/com/course/demo/EagerLazyDemo.java
@@ -0,0 +1,48 @@
+package com.course.demo;
+
+import com.course.entity.Course;
+import com.course.entity.Instructor;
+import com.course.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class EagerLazyDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                                                    .addAnnotatedClass(Instructor.class)
+                                                    .addAnnotatedClass(InstructorDetail.class)
+                                                    .addAnnotatedClass(Course.class)
+                                                    .buildSessionFactory();
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try
+        {
+
+            // start a transaction
+            session.beginTransaction();
+
+            int id = 1;
+            Instructor instructor = session.get(Instructor.class, id);
+
+            System.out.println("EAGER: ==== GET COURSE ===" + instructor);
+
+            System.out.println("EAGER: ==== GET INSTRUCTOR ===" + instructor.getCourses());
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("EAGER: Done!");
+        }
+        finally {
+            session.close();
+            factory.close();
+        }
+
+    }
+
+}
