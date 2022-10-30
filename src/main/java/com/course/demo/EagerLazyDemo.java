1375c549d76d60a95092d29840c6b6f97e0cc850
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-10-30 14:55:06 +0800

    solve  option 1 - call getter method while session open

--- src/main/java/com/course/demo/EagerLazyDemo.java
+++ src/main/java/com/course/demo/EagerLazyDemo.java
@@ -29,12 +29,19 @@ public class EagerLazyDemo {
             int id = 1;
             Instructor instructor = session.get(Instructor.class, id);
 
-            System.out.println("LAZY: ==== GET COURSE ===" + instructor);
+            System.out.println("LAZY: ==== GET INSTRUCTOR ===" + instructor);
 
-            System.out.println("LAZY: ==== GET INSTRUCTOR ===" + instructor.getCourses());
+            // To solve it, call getter method while session open
+            System.out.println("LAZY: ==== GET COURSES ===" + instructor.getCourses());
 
             // commit transaction
             session.getTransaction().commit();
+
+            // close session
+            session.close();
+
+            // this should throw exception if you try to get lazy loading data after session closed
+            System.out.println("LAZY: ==== GET COURSES ===" + instructor.getCourses());
 
             System.out.println("LAZY: Done!");
         }
