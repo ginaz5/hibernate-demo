144609ccc4c5ac3b4cc4a57781628bb10a3ae4ad
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-10-31 09:35:57 +0800

    change schema

--- src/main/java/com/course/jdbc/TestJdbc.java
+++ src/main/java/com/course/jdbc/TestJdbc.java
@@ -7,7 +7,7 @@ public class TestJdbc {
 
     public static void main(String[] args) {
 
-        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?userSSL=false";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-04-one-to-many-uni?userSSL=false";
         String user = "hbstudent";
         String pass = "hbstudent";
         try
