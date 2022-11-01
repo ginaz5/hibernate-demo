d450d1adddee1ae856efb54c3ddeb0e934d8a3cf
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-11-01 23:12:08 +0800

    create courses and add student

---
+++ src/main/java/com/course/demo/GetCoursesForMaryDemo.java
@@ -0,0 +1,49 @@
+package com.course.demo;
+
+import com.course.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetCoursesForMaryDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                                                    .addAnnotatedClass(Instructor.class)
+                                                    .addAnnotatedClass(InstructorDetail.class)
+                                                    .addAnnotatedClass(Course.class)
+                                                    .addAnnotatedClass(Review.class)
+                                                    .addAnnotatedClass(Student.class)
+                                                    .buildSessionFactory();
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try
+        {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the student mary from database
+            int studentId = 2;
+            Student student = session.get(Student.class, studentId);
+
+            System.out.println("Load student: " + student);
+            System.out.println("Courses: " + student.getCourses());
+
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        }
+        finally {
+            session.close();
+            factory.close();
+        }
+
+    }
+
+}
